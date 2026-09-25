91e393c0257519d6b96846c91b14f5c3b6d12ba2
zezva802 <zezvakobaidze19@gmail.com>
2026-09-25 21:59:34 +0400

    feat: add booking storage class

---
+++ src/BookCatalog.Api/Interfaces/IBookStorage.cs
@@ -0,0 +1,17 @@
+using BookCatalog.Api.Entities;
+
+namespace BookCatalog.Api.Interfaces;
+
+public interface IBookStorage
+{
+    Book? GetById(int id);
+
+    IEnumerable<Book> GetAll();
+
+    Book Create(string title, string? author, int? year);
+
+    Book? Update(int id, Book book);
+
+    void Delete(int id);
+
+}
