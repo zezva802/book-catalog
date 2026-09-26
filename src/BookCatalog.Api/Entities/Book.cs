c56b432ad49dbc18de31a445d7f183654bd8f1c6
zezva802 <zezvakobaidze19@gmail.com>
2026-09-26 20:40:22 +0400

    feat: add validations for book entity fields, + formatting

--- src/BookCatalog.Api/Entities/Book.cs
+++ src/BookCatalog.Api/Entities/Book.cs
@@ -1,9 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BookCatalog.Api.Entities;
 
-public class Book{
-    public int Id {get; set;}
-    public required string Title {get; set;}
-    public string? Author {get; set;}
-    public int? Year {get; set;}
-    
+public class Book : IValidatableObject
+{
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(200)]
+    public required string Title { get; set; }
+
+    [StringLength(100, MinimumLength = 2)]
+    public string? Author { get; set; }
+
+    [Range(-3500, int.MaxValue, ErrorMessage = "Year must be no earlier than 3500 BCE (-3500)")]
+    public int? Year { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext context)
+    {
+        if (Author != null && string.IsNullOrWhiteSpace(Author))
+        {
+            yield return new ValidationResult("Author name can not be only spaces", [nameof(Author)]);
+        }
+        int allowedYear = DateTime.UtcNow.Year + 1;
+        if (Year > allowedYear)
+        {
+            yield return new ValidationResult($"Year must be no later than {allowedYear}", [nameof(Year)]);
+        }
+    }
+
 }
